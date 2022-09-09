fc1ca71fa3ca132c9495ea3db6536eacc334fdfa
LAPTOP-FDJF2HJ0\omnicom <varun@kernellabs.io>
2022-09-09 11:11:30 -0700

    feat(fzf):loading fix

--- nvchad/lua/custom/plugins/init.lua
+++ nvchad/lua/custom/plugins/init.lua
@@ -11,6 +11,7 @@ return {
    },
    ["nvim-telescope/telescope-fzf-native.nvim"] = {
       run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+      after = "telescope.nvim",
       config = function()
       require('telescope').setup {
         extensions = {
